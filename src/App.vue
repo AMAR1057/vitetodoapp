7382eb27fd5947f0b2bd6ce131d8ebfd690ffa17
amar1057 <amar1057@stu.southtexascollege.edu>
2021-12-04 21:59:22 -0600

    final with updates

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup>
-
 import Title from "./components/Title.vue";
 import List from "./components/List.vue";
 </script>
 
-
-
-
-
-
-
-
 <template>
-  <div class="min-h-screen bg-blue-900">
+  <div class="min-h-screen bg-blue-700">
     <div class="container flex flex-col pt-8 mx-auto space-y-8">
       <Title />
-          
+
       <div class="flex justify-around">
-       <List />
+        <List />
         <List isCompleted />
       </div>
     </div>
